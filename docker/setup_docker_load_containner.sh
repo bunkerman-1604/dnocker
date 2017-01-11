cf2dfad6566c617a194d3f0bd7d5e4f86ee39727
bunkerman-1604 <simul2013@163.com>
2017-01-11 17:55:14 +0800

    Update setup_docker_load_containner.sh
    for ubuntu 16.04 apt-sourcelist

--- docker/setup_docker_load_containner.sh
+++ docker/setup_docker_load_containner.sh
@@ -53,3 +53,7 @@ fi
 #docker for port/memory/volume share                    #
 #-m 2000m \                                             #
 #########################################################
+#deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+#deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+#deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+#deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
