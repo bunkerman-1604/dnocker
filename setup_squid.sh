10ab62a6b0013cd1dbd6c05c6d2d0d5db791f329
bunkerman-1604 <simul2013@163.com>
2017-02-09 16:02:03 +0800

    Update setup_squid.sh
    add authorize setting

--- setup_squid.sh
+++ setup_squid.sh
@@ -9,4 +9,13 @@ fi
 sed -i "/^acl CONNECT/,+0aacl AllowIP src ${AllowIP}"            /etc/squid3/squid.conf
 ed -i "/^http_access deny all/c#http_access deny all"            /etc/squid3/squid.conf
 sed -i "/^#http_access deny all/,+0a http_access allow all"       /etc/squid3/squid.conf
+
+#touch /etc/squid3/squid_passwd
+#htpasswd /etc/squid3/squid_passwd testname
+#auth_param basic program /usr/lib/squid3/basic_ncsa_auth /etc/squid3/squid_passwd
+#auth_param basic children 5 startup=5 idle=1
+#auth_param basic realm Squid proxy-caching web server
+#auth_param basic credentialsttl 2 hours
+#acl auth_user proxy_auth sam
+#http_access allow auth_user
 service squid3 restart
