7fa7ee12e32b055864d712f404ae89401ec54218
bunkerman-1604 <simul2013@163.com>
2017-07-28 17:06:37 +0800

    Update setup_squid.sh
    fix "s" at line 10

--- setup_squid.sh
+++ setup_squid.sh
@@ -7,7 +7,7 @@ else
         cp /etc/squid3/squid.conf.bak /etc/squid3/squid.conf
 fi
 sed -i "/^acl CONNECT/,+0aacl AllowIP src ${AllowIP}"            /etc/squid3/squid.conf
-ed -i "/^http_access deny all/c#http_access deny all"            /etc/squid3/squid.conf
+sed -i "/^http_access deny all/c#http_access deny all"            /etc/squid3/squid.conf
 sed -i "/^#http_access deny all/,+0a http_access allow all"       /etc/squid3/squid.conf
 
 #touch /etc/squid3/squid_passwd
